723e0db856de02989a86624a9a7c35f4a74f3688
sulav <sulavmaharjan63@gmail.com>
2021-03-14 10:15:12 +0545

    Added comment form

--- blog/models/comment_model.py
+++ blog/models/comment_model.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from blog.models import Blog
 class Comment(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     body = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
     
-    
+
     
