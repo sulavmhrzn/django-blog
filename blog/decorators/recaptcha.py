27553bd6307552af131e6c02b9cf45ab5054241c
sulav <sulavmaharjan63@gmail.com>
2021-03-14 12:21:56 +0545

    Created decorator for captcha validation

---
+++ blog/decorators/recaptcha.py
@@ -0,0 +1,22 @@
+import requests
+from django.conf import settings
+from django.contrib import messages
+
+def check_recaptcha(func):
+    def wrapped_view(request, *args, **kwargs):
+        request.recaptcha_is_valid = None
+        if request.method == 'POST':
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            data = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+            result = r.json()
+            if result['success']:
+                request.recaptcha_is_valid = True
+            else:
+                request.recaptcha_is_valid = False
+                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+        return func(request, *args, **kwargs)
+    return wrapped_view
--- blog/views.py
+++ blog/views.py
@@ -6,41 +6,31 @@ from django.conf import settings
 from blog.models import Blog
 from blog.models import Comment
 from blog.forms import CommentForm
-
+from blog.decorators.recaptcha import check_recaptcha
 class HomePageView(ListView):
     queryset = Blog.published.all() 
     template_name = 'blog/blogs.html'
     context_object_name = 'blogs'
 
+@check_recaptcha
 def detail_blog(request, pk, author, slug):
     blog = get_object_or_404(Blog, pk=pk, author__username=author, slug=slug, status='PU')
     comments = Comment.objects.filter(blog=blog).order_by('-date_added')
-    form = CommentForm()
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
-        if form.is_valid():
-            """Begin recaptcha validation"""
-            recaptcha_response = request.POST.get('g-recaptcha-response')
-            data = {
-                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-                'response': recaptcha_response
-            }
-            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-            result = r.json()
-            """ End recaptcha valdation"""
-            if result['success']:
-                add_blog = form.save(commit=False)
-                add_blog.blog = blog
-                add_blog.save()
-                messages.success(request, 'Comment successfully added.')
-                return redirect(request.path)
-            else:
-                messages.error(request, 'Invalid reCAPTCHA. Please try again')
+        if form.is_valid() and request.recaptcha_is_valid:
+            add_blog = form.save(commit=False)
+            add_blog.blog = blog
+            add_blog.save()
+            messages.success(request, 'Comment successfully added.')
+            return redirect(request.path)
         else:
             messages.error(request, 'Something went wrong.')
             return redirect(request.path)
-
+    else:
+        form = CommentForm()
+    
     context = {
         'blog':blog,
         'comments':comments,
